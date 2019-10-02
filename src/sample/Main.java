86e477f48b30338cc8a8aa316783a1c5632d5de9
Martin Blomberg <martin.blomberg@outlook.com>
2019-10-02 10:01:54 +0200

    fxml updated

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,9 +13,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        FlowPane root = new FlowPane();
+//        Button button = new Button("Hej");
+//        root.getChildren().add(button);
+//        Button button;
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+
     }
 
 
