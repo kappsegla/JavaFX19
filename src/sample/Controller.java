6251fd4697beeca4f625efffe46e5596e9f84ddb
Martin Blomberg <martin.blomberg@outlook.com>
2019-10-02 14:29:25 +0200

    ListView

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
@@ -20,31 +17,43 @@ public class Controller {
     TextArea textArea;
     @FXML
     CheckBox checkBox1;
+    @FXML
+    Circle circle;
+    @FXML
+    Slider slider;
+    @FXML
+    ListView<String> listView;
+
 
     Model model = new Model();
 
-    public Controller(){
+    public Controller() {
         //@FXML marked fields are still null do nothing here
     }
 
-    public void initialize(){
+    public void initialize() {
         //Will run after all fields are set and view is ready
         //Next line replaces onAction="#button1Action" in fxml file
         //button1.addEventHandler(ActionEvent.ACTION,this::button1Action);
-        textArea.textProperty().bindBidirectional( model.textProperty());
+        textArea.textProperty().bindBidirectional(model.selectedItemProperty());
         model.textProperty().bindBidirectional(textField.textProperty());
-        checkBox1.selectedProperty().bindBidirectional(model.enabledProperty());
-        textField.disableProperty().bind(model.enabledProperty());
-        textArea.disableProperty().bind(model.enabledProperty());
+//        checkBox1.selectedProperty().bindBidirectional(model.enabledProperty());
+//        textField.disableProperty().bind(model.enabledProperty());
+//        textArea.disableProperty().bind(model.enabledProperty());
+        checkBox1.selectedProperty().bindBidirectional(textField.disableProperty());
+        slider.valueProperty().bindBidirectional(circle.radiusProperty());
 
-
-
+        listView.setItems(model.getItems());
+        model.selectedItemProperty().bind(listView.getSelectionModel().selectedItemProperty());
 
     }
 
     public void button1Action(ActionEvent actionEvent) throws InterruptedException {
-        model.setText("");
-       // textArea.setText(model.getText());
+      //   model.setText("");
+      //  model.setEnabled(false);
+        // textArea.setText(model.getText());
+        model.getItems().add(textField.getText());
+
 
     }
 
--- src/sample/Model.java
+++ src/sample/Model.java
@@ -4,6 +4,11 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Model {
 
@@ -33,5 +38,26 @@ public class Model {
 
     public void setEnabled(boolean enabled) {
         this.enabled.set(enabled);
+    }
+
+    private ObservableList<String> items = FXCollections.observableArrayList (
+            "A", "B", "C", "D", "E");
+
+    public ObservableList<String> getItems() {
+        return items;
+    }
+
+    private StringProperty selectedItem = new SimpleStringProperty();
+
+    public String getSelectedItem() {
+        return selectedItem.get();
+    }
+
+    public StringProperty selectedItemProperty() {
+        return selectedItem;
+    }
+
+    public void setSelectedItem(String selectedItem) {
+        this.selectedItem.set(selectedItem);
     }
 }
