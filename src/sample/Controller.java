0a980dcb0866a455497372799b4f6ab6e087a03d
Martin Blomberg <martin.blomberg@outlook.com>
2019-10-02 10:39:28 +0200

    Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,38 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+
 public class Controller {
+
+    @FXML
+    Button button1;
+
+    @FXML
+    TextField textField;
+
+    public Controller(){
+        //button1 still null
+    }
+
+    public void initialize(){
+        //Will run after all fields are set and view is ready
+
+
+    }
+
+    public void button1Action(ActionEvent actionEvent) {
+        button1.setText("New Text");
+        button1.setDisable(true);
+    }
+
+    public void circleClicked(MouseEvent mouseEvent) {
+        Circle circle = (Circle) mouseEvent.getSource();
+        circle.setRadius(40.0);
+        button1.setDisable(false);
+    }
 }
